4c1f1afff687db22700445b7aaa2299ac8a88c6b
seokahi <skh9797@naver.com>
2022-01-02 02:35:51 +0900

    step1

---
+++ 새 폴더/step1.py
@@ -0,0 +1,42 @@
+#1
+# # print("Hello World!")
+#2
+# print("강한친구 대한육군\n강한친구 대한육군")
+# print("강한친구 대한육군")
+# print("강한친구 대한육군")
+#3
+# print("\    /\ \n )  ( ')\n(  /  )\n \(__)|")
+#4
+# print('|\_/| \n|q p|   /} \n( 0 )"""\ \n|"^"`    | \n||_/=\\\__|')
+# #5
+# A,B=map(int,input().split())
+# print(A+B)
+#6
+# A,B=map(int,input().split())
+# print(A-B)
+# 7
+# A,B=map(int,input().split())
+# print(A*B)
+#8
+# A,B=map(int,input().split())
+# print(A/B)
+#9
+# A,B=map(int,input().split())
+# print(A+B)
+# print(A-B)
+# print(A*B)
+# print(A//B)
+# print(A%B)
+#10
+# A,B,C=map(int,input().split())
+# print((A+B)%C)
+# print(((A%C) + (B%C))%C)
+# print((A*B)%C)
+# print(((A%C) * (B%C))%C)
+#11
+# A=int(input())
+# B=input()
+# print(A*int(B[2]))
+# print(A*int(B[1]))
+# print(A*int(B[0]))
+# print(A*int(B))
